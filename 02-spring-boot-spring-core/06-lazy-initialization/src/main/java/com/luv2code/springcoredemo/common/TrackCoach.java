08681a191173815542675be0d25c7b3bbc5f3a69
Joel Neto <joel.antoniete@gmail.com>
2024-06-18 18:05:20 -0300

    "Lazy Construction"

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -1,9 +1,11 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy //apenas essa classe fica com a iniciação lenta
 public class TrackCoach implements Coach {
 
     public TrackCoach() {
