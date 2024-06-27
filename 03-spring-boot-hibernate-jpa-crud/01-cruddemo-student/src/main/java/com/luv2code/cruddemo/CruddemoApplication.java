a74cf2aeba5e828290547a968a11ee32db3b5a92
Joel Neto <joel.antoniete@gmail.com>
2024-06-26 22:24:07 -0300

    "setando multiplos Students"

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -16,23 +16,36 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
-		// create  a student object
+		// create the student object
 		System.out.println("Creating new student object ...");
-		Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
 
-		// save the student
+		// save the student object
 		System.out.println("Saving the student ...");
 		studentDAO.save(tempStudent);
 
 		// display id of the saved student
-		int theId = tempStudent.getId();
-		System.out.println("Saved student. Generated id: " + theId);
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 
+	}
+	private void createMultipleStudents(StudentDAO studentDAO) {
+
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
+
+		// save the student objects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 }
