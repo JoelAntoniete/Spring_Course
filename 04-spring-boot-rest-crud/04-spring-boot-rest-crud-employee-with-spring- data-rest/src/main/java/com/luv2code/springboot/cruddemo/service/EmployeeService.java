b4997c7a7c9dfabd361e9251f4ebb6374db09336
Joel Neto <joel.antoniete@gmail.com>
2024-07-16 21:13:16 -0300

    "Frist: projeto Spring JPA data"

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring- data-rest/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring- data-rest/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -1,12 +1,14 @@
-package com.luv2code.springboot.cruddemo.dao;
+package com.luv2code.springboot.cruddemo.service;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 import java.util.List;
 
-public interface EmployeeDAO {
+public interface EmployeeService {
+
     List<Employee> findAll();
     Employee findByid(int theId);
     Employee save(Employee employee);
     void delete(int theId);
+
 }
