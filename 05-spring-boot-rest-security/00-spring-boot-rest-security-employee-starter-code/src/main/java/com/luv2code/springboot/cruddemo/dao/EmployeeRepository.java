ac8fd9d7efff1ddff57327ae334a14a82cc87de3
Joel Neto <joel.antoniete@gmail.com>
2024-07-18 19:12:39 -0300

    "Criacao do modulo Spring security"

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,8 +3,8 @@ package com.luv2code.springboot.cruddemo.dao;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//podemos mudar o caminho
-//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    //Extendendo JpaRepository nao precisa configurar o crud manualmente
+
+    // that's it ... no need to write any code LOL!
+
 }
