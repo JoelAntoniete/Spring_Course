d2468af8c850b3e820cd4e383358a5edeec4cd8b
Joel Neto <joel.antoniete@gmail.com>
2025-02-03 16:34:34 -0300

    aula 183 completa

--- 06-spring-boot-springMVC/01-thynmeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-springMVC/01-thynmeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,21 +1,30 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class StudentController {
-
+    //para pegar a lista de paises de properties precisamos fazer uma lista, e colocar a annotation @Value
+    @Value("${countries}")
+    private List<String> countries ;
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 
         Student theStudent = new Student();
 
         theModel.addAttribute("student", theStudent);
+
+        //adicionar a lista no model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
