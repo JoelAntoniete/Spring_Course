0760c8d4749beb774ea1829d31b362f2a1b596ed
Joel Neto <joel.antoniete@gmail.com>
2025-02-03 14:57:36 -0300

    Aula 172

--- 06-spring-boot-springMVC/01-thynmeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-springMVC/01-thynmeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -26,6 +27,18 @@ public class HelloWorldController {
         String result = "Yo! " + theName;
 
         model.addAttribute("menssage", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName,
+                                          Model model) {
+
+        //@RequestParam ele pega o parametro do formularion, que tiver a tag com o nome que colocarmos como arg
+        theName = theName.toUpperCase();
+        String result = "Hey My Friend from v3! " + theName;
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
