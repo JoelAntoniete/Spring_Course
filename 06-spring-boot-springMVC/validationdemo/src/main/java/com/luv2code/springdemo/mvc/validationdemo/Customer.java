f3074106100ef517cd7f30f6fb2dbfd22ade48a4
Joel Neto <joel.antoniete@gmail.com>
2025-02-04 12:12:25 -0300

    "Aula  ate 203"

--- 06-spring-boot-springMVC/validationdemo/src/main/java/com/luv2code/springdemo/mvc/validationdemo/Customer.java
+++ 06-spring-boot-springMVC/validationdemo/src/main/java/com/luv2code/springdemo/mvc/validationdemo/Customer.java
@@ -1,9 +1,6 @@
 package com.luv2code.springdemo.mvc.validationdemo;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -14,6 +11,17 @@ public class Customer {
     @Min(value=0, message="must be greater tham or equal to zero")
     @Max(value=10, message="must be less tham or equal to zero")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
